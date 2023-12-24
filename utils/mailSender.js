b78b70ab1270ecb7e82dc8faf8aab5f4a3fbbeba
Mohit kumar <64029983+mohitkumar000@users.noreply.github.com>
2023-12-24 15:48:52 +0530

    all routes created

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -12,7 +12,7 @@ const mailSender = async (email, title, body) => {
 
 
             let info = await transporter.sendMail({
-                from: 'Edu_Notion || by Babbar',
+                from: 'StudyNotion || CodeHelp - by Babbar',
                 to:`${email}`,
                 subject: `${title}`,
                 html: `${body}`,
